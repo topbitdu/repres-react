56f68ecad93b03ceca43f408c31777c9f41a5ec3
Topbit Du <topbit.du@gmail.com>
2017-01-18 11:30:19 +0800

    1, Improve the Application helper for the document.

--- app/helpers/repres/react/application_helper.rb
+++ app/helpers/repres/react/application_helper.rb
@@ -3,6 +3,8 @@
 
 module Repres::React::ApplicationHelper
 
+  ##
+  # 调用 repres/react/script 这个局部脚本， options 是传递的参数。
   def react_script(options = {})
     render partial: 'repres/react/script', locals: { options: options }
   end
